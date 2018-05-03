26be553ef36f33c273ce05bb188a648950b49504
HSaad <hagrsaad16@gmail.com>
2018-05-03 12:22:48 -0400

    Complete 04 select within select tutorial

---
+++ 05_sum_and_count.sql
@@ -0,0 +1,41 @@
+-- 1. Total world population
+/*
+	Show the total population of the world.
+*/
+
+-- 2. List of continents.
+/*
+	List all the continents- just once each.
+*/
+
+-- 3. GDP of Africa
+/*
+	Give the total GDP of Africa.
+*/
+
+-- 4. Count the big countries
+/*
+	How many countries have an area of at least 1000000.
+*/
+
+-- 5. Baltic states population
+/*
+	What is the total population o f('Estonia', 'Latvia', 'Lithuania')
+*/
+
+-- 6. Counting the countries of each continent
+/*
+	For each continent show the continent and number of countries
+*/
+
+-- 7. Counting big countries in each continent
+/*
+	For each continent show the continent and number of countries
+	with populations of at least 10 million.
+*/
+
+-- 8. Counting big continents
+/*
+	List the continents that have a total population of at least 100
+	million.
+*/
---
+++ 07_more_join_operations.sql
@@ -0,0 +1,82 @@
+-- 1. 1962 movies
+/*
+	List the films where the yr is 1962
+*/
+
+-- 2.When was Citizen Kand released?
+/*
+	Give year of 'Citizen Kane'
+*/
+
+-- 3. Star Trek movies
+/*
+	List all of the Star Trek movies, including the id, title, and yr
+	(all of these movies include the words Star Trek in the title). Order
+	results by year.
+*/
+
+-- 4. id for actor Glenn Close
+/*
+	What id number does the actor 'Glenn Close' have?
+*/
+
+-- 5. id for Casablanca
+/*
+	What is the id of the film 'Casablanca'
+*/
+
+-- 6. Cast list for Casablanca
+/*
+	Obtain the cast list for 'Casablanca'
+*/
+
+-- 7. Alien cast list
+/*
+	Obtain the cast list for the film 'Alien'
+*/
+
+-- 8. Harrison Ford movies
+/*
+	List the films in which 'Harrison Ford' has appeared
+*/
+
+-- 9. Harrision Ford as a supporting actor
+/*
+	List the films where 'Harrsion Ford' has appeared -
+	but not in the staring role.
+*/
+
+-- 10. Lead actores in 1962 movies
+/*
+	List the films together with the leading star for all 1962 films.
+*/
+
+-- 11. Busy years for John Travolta
+/*
+	Which were the busiest years for 'John Travolta', show the year and 
+	the number of movies he made each year for any year in which he
+	made more than 2 movies. 
+*/
+
+-- 12. Lead actor in Julie Andrews movies
+/*
+	List the film title and the leading actor for all of the films 
+	'Julie Andrews' played in. 
+*/
+
+-- 13. Actors with 30 leading roles
+/*
+	Obtain a list, in alphabetical order, of actors who've had at least 30
+	starring roles.
+*/
+
+-- 14. 
+/*
+	List the films released in the year 1978 ordered by the number of
+	actors in the cast, then by title.
+*/
+
+-- 15.
+/*
+	List all the people who have worked with 'Art Garfunkel'
+*/
