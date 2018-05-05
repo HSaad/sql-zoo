56f88992af80a0a7b9f77bbacab54f3aacb6ee1b
HSaad <hagrsaad16@gmail.com>
2018-05-05 11:08:38 -0400

    Complete NSS tutorial 8+

--- 08+_numeric_examples.sql
+++ 08+_numeric_examples.sql
@@ -3,17 +3,34 @@
 	Show the percentage who STRONGLY AGREE
 */
 
+SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science';
+
 -- 2. Calculate how many agree or strongly agree
 /*
 	Show the institution and subject where the schore is at least
 	100 for question 15.
 */
 
+SELECT institution, subject
+  FROM nss
+ WHERE question='Q15'
+   AND score >= 100;
+
 -- 3. Unhappy Computer Students
 /*
 	Show the institution and score where the score for '(8)
 	Computer Science' is less than 50 or question 'Q15'
 */
+
+SELECT institution, score
+  FROM nss
+ WHERE question='Q15'
+   AND score < 50
+   AND subject='(8) Computer Science';
 
 -- 4. More Computing or Creative Students?
 /*
@@ -22,12 +39,26 @@
 	Computer Science' and '(H) Creative Arts and Design'.
 */
 
+SELECT subject,SUM(response)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(H) Creative Arts and Design'
+   OR subject='(8) Computer Science')
+ GROUP BY subject;
+
 -- 5. Strongly Agree Numbers
 /*
 	Show the subject and total number of students who
 	A_STRONGLY_AGREE to question 22 for each of the subjects
 	'(8) Computer Science' and '(H) Creative Arts and Design'
 */
+
+SELECT subject,SUM(response * A_STRONGLY_AGREE / 100)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(H) Creative Arts and Design'
+   OR subject='(8) Computer Science')
+ GROUP BY subject;
 
 -- 6. Strongly Agree, Percentage
 /*
@@ -37,11 +68,24 @@
 	Design'.
 */
 
+SELECT subject, ROUND(SUM(A_STRONGLY_AGREE * response) / SUM(response))
+  FROM nss
+  WHERE question='Q22'
+    AND (subject='(H) Creative Arts and Design'
+    OR subject='(8) Computer Science')
+GROUP BY subject;
+
 -- 7. Scores for Institutions in Manchester
 /*
 	Show the average scores for question 'Q22' for each
 	institution that include 'Manchester' in the name. 
 */
+
+SELECT institution, ROUND(SUM(score * response) / SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution;
 
 -- 8. Number of Computing Students in Manchester
 /*
@@ -49,3 +93,9 @@
 	of computing students for institutions in Manchester for
 	'Q01'.
 */
+
+SELECT institution, SUM(sample), SUM(CASE WHEN subject = '(8) Computer Science' THEN sample ELSE 0 END) AS comp
+  FROM nss
+ WHERE question='Q01'
+   AND (institution LIKE '%Manchester%')
+ GROUP BY institution;
